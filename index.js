7191e8ea51e722b8eed299fda8bcf2a5257df933
curvingchicken <thedude6583@gmail.com>
2021-04-06 23:06:27 +0900

    test

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 #!/usr/bin/env node
 "use strict";
-exports.__esModule = true;
-var fs_1 = require("fs");
-var path_1 = require("path");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs_1 = __importDefault(require("fs"));
+var path_1 = __importDefault(require("path"));
 var discordDir = '/home/pi/CompilerDiscord';
-var logFilePath = path_1["default"].resolve(discordDir, 'log');
-var adminlogFilePath = path_1["default"].resolve(discordDir, 'adminlog');
-fs_1["default"].watchFile(logFilePath, function (curr, prev) {
+var logFilePath = path_1.default.resolve(discordDir, 'log');
+var adminlogFilePath = path_1.default.resolve(discordDir, 'adminlog');
+fs_1.default.watchFile(logFilePath, function (curr, prev) {
     console.log(curr.atimeMs);
 });
-fs_1["default"].watchFile(adminlogFilePath, function (curr, prev) {
+fs_1.default.watchFile(adminlogFilePath, function (curr, prev) {
     console.log(curr.atimeMs);
 });
