bb9c18aefd5087a8a45717851f5c3b9fb2f0e9f4
curvingchicken <thedude6583@gmail.com>
2021-04-06 23:49:22 +0900

    test

--- index.js
+++ index.js
@@ -9,9 +9,36 @@ var path_1 = __importDefault(require("path"));
 var discordDir = '/home/pi/CompilerDiscord';
 var logFilePath = path_1.default.resolve(discordDir, 'log');
 var adminlogFilePath = path_1.default.resolve(discordDir, 'adminlog');
+var logFileSize = 0;
+var adminlogFileSize = 0;
+fs_1.default.readFile(logFilePath, function (err, data) {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        logFileSize = data.toString().length;
+    }
+});
+fs_1.default.readFile(adminlogFilePath, function (err, data) {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        adminlogFileSize = data.toString().length;
+    }
+});
 fs_1.default.watchFile(logFilePath, function (curr, prev) {
-    var time = new Date(Math.floor(curr.mtimeMs));
-    console.log(curr.atimeMs, time.getMinutes());
+    fs_1.default.readFile(logFilePath, function (err, data) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            var time = new Date(Math.floor(curr.mtimeMs));
+            var fileChange = data.toString().slice(logFileSize);
+            console.log(fileChange);
+            console.log(curr.atimeMs, time.getMinutes());
+        }
+    });
 });
 fs_1.default.watchFile(adminlogFilePath, function (curr, prev) {
     console.log(curr.atimeMs);
--- index.ts
+++ index.ts
@@ -7,9 +7,44 @@ const discordDir = '/home/pi/CompilerDiscord';
 const logFilePath = path.resolve(discordDir, 'log');
 const adminlogFilePath = path.resolve(discordDir, 'adminlog');
 
+let logFileSize = 0;
+let adminlogFileSize = 0;
+
+fs.readFile(logFilePath, (err, data) => {
+    if(err)
+    {
+        console.log(err);
+    }
+    else
+    {
+        logFileSize = data.toString().length;
+    }
+});
+
+fs.readFile(adminlogFilePath, (err, data) => {
+    if(err)
+    {
+        console.log(err);
+    }
+    else
+    {
+        adminlogFileSize = data.toString().length;
+    }
+});
+
 fs.watchFile(logFilePath, (curr, prev) => {
-    let time = new Date(Math.floor(curr.mtimeMs));
-    console.log(curr.atimeMs, time.getMinutes());
+    fs.readFile(logFilePath, (err, data) => {
+        if(err){
+            console.log(err);
+        }
+        else
+        {
+            let time = new Date(Math.floor(curr.mtimeMs));
+            let fileChange = data.toString().slice(logFileSize);
+            console.log(fileChange);
+            console.log(curr.atimeMs, time.getMinutes());
+        }
+    })
 });
 
 fs.watchFile(adminlogFilePath, (curr, prev) => {
